265c1a0eb559e0370678bba66f50dc79f423cd33
Amal Qasim <amal.qasim@ufst.dk>
2021-08-18 15:36:39 +0200

    funktioner kan opret traestammen og stampel det

---
+++ src/treetrunk.cljs
@@ -0,0 +1,30 @@
+(ns treetrunk
+  (:require [simulation :as sim]))
+
+(defn create-tree-trunk
+  "This function is able to give a unique id for tree trunk and register the global start time of this tree trunk."
+  []
+  {:id           (cljs.core/random-uuid)
+   :global-start-time @sim/sim-time})
+
+(defn stample-start-time
+  "The function is able to register a start time to a tree trunk with given work station id."
+  [treetrunk ws-id]
+  (assoc-in treetrunk
+            [ws-id :start-time] @sim/sim-time))
+
+(defn stample-end-time
+  "The function is able to register a end time to a tree trunk with given work station id."
+  [treetrunk ws-id]
+  (assoc-in treetrunk
+            [ws-id :end-time] @sim/sim-time))
+
+(defn stample-global-end-time
+  "The function is able to register a global end time to a tree trunk."
+  [treetrunk]
+  (assoc-in treetrunk
+             :global-end-time @sim/sim-time))
+
+
+
+
